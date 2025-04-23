e971d9cb879bda6078a93e85b35ba49c24842976
suchetrana <149752649+suchetrana@users.noreply.github.com>
2025-04-23 09:41:40 +0530

    Create height of tree.java

---
+++ height of tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(maxDepth(root.left),maxDepth(root.right) ) + 1;
+    }
+}
